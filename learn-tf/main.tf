733d4a8acb9f505ceea48025b1531b9c0c7e12ff
noury <noury@khayat.cloud>
2021-09-12 11:32:57 +0200

    add variables and outputs to tf script

--- learn-tf/main.tf
+++ learn-tf/main.tf
@@ -15,7 +15,8 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
+  #  name     = "myTFResourceGroup"
+  name     = var.resource_group_name
   location = "westus2"
 
   tags = {
